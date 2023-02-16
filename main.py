c37d93c48e4822734ef4ece7ccc9148efb4da40a
Valériane Evin <121805896+ValerianeE@users.noreply.github.com>
2023-02-16 12:09:23 +0100

    Ajouts test unitaire et github actions

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 # Import libraries
-#import numpy as np
 import pandas as pd
 from flask import Flask, request
 import pickle
 import shap
-#import json
-import unittest
 
 app = Flask(__name__)
 
 #Définition chemin d'accès aux fichiers
 #PATH='C:/Users/valev/Projet-7/repo_git_api/'
 
-#Test unitaire qui vérifie si la prédiction renvoyée par la fonction est bien une valeur entre 0 et 1
-#def value_predict(self):
-#    if self.assertTrue(self,0<=y<=1): #doit être entre 0 et 1
-#        return True
-#    return False
-
 # Load data
 read_csv = pd.read_csv('sampled.csv',delimiter= ',')
-#read_csv=read_csv.drop(columns='index')
 read_csv.set_index("SK_ID_CURR", drop=False, inplace=True)
-print (read_csv)
 
 # Load the model
 model = pickle.load(open('model.pkl','rb'))
@@ -39,18 +28,15 @@ def predict():
     print("test id " , X_id)
     prediction = model.predict_proba(X_id)[:,1]
     print(prediction)
-    #value_predict(float(prediction))
     # SHAP values
     explainer = shap.TreeExplainer(model)
     shap_values = explainer(read_csv)
 
-    explanation_val = explainer.shap_values(X_id)
     sv=pd.DataFrame(shap_values[0].values[:,0])
     ev=explainer.expected_value[0]
-    return  '[{},{},{},{}]'.format(float(prediction), 
+    return  '[{},{},{}]'.format(float(prediction), 
                                         sv.to_json(), 
-                                        ev, 
-                                        pd.Series(explanation_val).to_json()) 
+                                        ev,) 
 
 def getScoreUtilisateur(numeroUtilisateur):
     # Make prediction using model loaded from disk as per the data.
@@ -66,10 +52,3 @@ if __name__ == '__main__':
 def hello_world():
     return "<p>Hello, World!</p>"
 
-#class TestStringMethods(unittest.TestCase):
-#
-#    def testavecUtilisateur100002(self): 
-#        scoreUtilisateur = getScoreUtilisateur(100002)
-#        self.assertGreaterEqual(scoreUtilisateur, 0)
-#        self.assertLessEqual(scoreUtilisateur, 1)
-        
