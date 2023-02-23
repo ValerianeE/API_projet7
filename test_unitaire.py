87d4fb9c435aa53f2dab30c5e6780d650b6e3519
Valériane Evin <121805896+ValerianeE@users.noreply.github.com>
2023-02-23 17:03:06 +0100

    update test_unitaire

--- test_unitaire.py
+++ test_unitaire.py
@@ -1,5 +1,6 @@
 import unittest
-from main import getScoreUtilisateur
+#from main import getScoreUtilisateur
+import main
 
 #Numéro identifiant du client dont on teste le score
 NUM_ID = 100002
@@ -8,7 +9,7 @@ class TestScore(unittest.TestCase):
 
     #Fonction qui teste si le score renvoyé est bien entre 0 et 1
     def test_is_score_between_0_and_1(self):  
-        score=getScoreUtilisateur(NUM_ID)
+        score=main.getScoreUtilisateur(NUM_ID)
         self.assertGreaterEqual(score, 0)
         self.assertLessEqual(score, 1)
         print("Le score est bien entre 0 et 1")
